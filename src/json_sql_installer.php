ecc69be38ed27b3bd2b946eb4e7acc80537e287d
Georg Semmler <Georg_semmler_05@web.de>
2012-05-04 15:44:34 +0200

    Install-Klasse hinzugefügt

---
+++ src/json_sql_installer.php
@@ -0,0 +1,79 @@
+<?php
+class jsonSqlInstaller{
+
+	private $dbh;
+
+	private $tables;
+
+	public _construct($input_tables,$input_dbh){
+		$this->tables=json_decode($input_tables);
+		$this->dbh=input_dbh;
+	}
+
+	private function getFieldType($type) {
+		$sql_type='';
+		if(is_array($type)) {
+			switch($type[0]) {
+				case "string":
+					$sql_type=' VARCHAR('.intval($type[1]).')';
+					break;
+				case "pointer":
+				case 'foreign':
+					$sql_type=' INT';
+					break;
+			}
+		} else {
+			switch($type) {
+				case "id":
+					$sql_type=' INT PRIMARY KEY AUTO_INCREMENT';
+					break;
+				case "int":
+					$sql_type=' INT';
+					break;
+				case 'date':
+					$sql_type=' DATE';
+					break;
+				case 'datetime':
+					$sql_type=' DATETIME';
+					break;
+				case 'bool':
+				case 'boolean':
+					$sql_type=' BOOLEAN';
+					break;
+				case 'text':
+				case 'json':
+					$sql_type=' TEXT';
+					break;
+				case 'timestamp':
+					$sql_type=' TIMESTAMP';
+					break;
+			}
+		}
+		return $sql_type;
+	}
+	public function install(){
+		$query='';
+		foreach($this->tables as $name=>$fields) {
+			$query.='DROP TABLE IF EXISTS '.$name.";\n";
+			$query.='CREATE TABLE '.$name." (\n";
+			$i=0;
+			foreach($fields as $fname=>$type) {
+				if($i!=0)
+					$query.=",\n";
+				$query.=' '.$fname.' '.getFieldType($type);
+				if($type=='int'&&$fname=='id')
+					$query.=' PRIMARY KEY ';
+				++$i;
+			}	
+			$query.="\n);\n";
+		}
+		try {
+			$e=$this->dbh->exec($query);
+			echo '<br>Database created <br />';
+		}catch(Exception $e) {
+			echo 'Fehler: '.$e;
+		}
+	}
+}
+?>
+
