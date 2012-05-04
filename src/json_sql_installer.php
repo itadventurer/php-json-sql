3ec8fc98894e95799f24bec18037aefb5498ca7c
Georg Semmler <Georg_semmler_05@web.de>
2012-05-04 16:09:59 +0200

    Fehler entfernt, kommentiert(jsonSqlInstaller)

--- src/json_sql_installer.php
+++ src/json_sql_installer.php
@@ -1,15 +1,29 @@
 <?php
 class jsonSqlInstaller{
-
+	/**
+	 * The PDO-Database-Class
+	 * @var pdo
+	 */
 	private $dbh;
-
+	/**
+	 * The Database-Structure
+	 * @var json_object
+	 */
 	private $tables;
-
-	public _construct($input_tables,$input_dbh){
+	/**
+	 * Initialisiert die Klasse
+	 * @param String $input_tables JSON_String mit Datenbank-Structur
+	 * @param pdo $input_dbh	pdo-Objekt
+	 */
+	public function __construct($input_tables,$input_dbh){
 		$this->tables=json_decode($input_tables);
-		$this->dbh=input_dbh;
+		$this->dbh=$input_dbh;
 	}
-
+	/**
+	 * Konvertiert den im JSON-Objekt $tables festgelegten Datentyp in den Passenden Mysql-Datentyp
+	 * @param String $type Typ aus $tables
+	 * @return String mit dem Mysql-Datentyp
+	 */
 	private function getFieldType($type) {
 		$sql_type='';
 		if(is_array($type)) {
@@ -51,6 +65,9 @@ class jsonSqlInstaller{
 		}
 		return $sql_type;
 	}
+	/**
+	 * Funktion legt die in $tables beschriebene Datenbank an
+	 */
 	public function install(){
 		$query='';
 		foreach($this->tables as $name=>$fields) {
@@ -60,7 +77,7 @@ class jsonSqlInstaller{
 			foreach($fields as $fname=>$type) {
 				if($i!=0)
 					$query.=",\n";
-				$query.=' '.$fname.' '.getFieldType($type);
+				$query.=' '.$fname.' '.$this->getFieldType($type);
 				if($type=='int'&&$fname=='id')
 					$query.=' PRIMARY KEY ';
 				++$i;
